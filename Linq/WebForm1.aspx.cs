9bfa269d58bc9e89a66b1bbf66c405edc9cd73b9
chris <chris@DEVBOX>
2017-04-20 23:22:24 -0700

    initial

---
+++ Linq/WebForm1.aspx.cs
@@ -0,0 +1,24 @@
+﻿using Linq.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Linq
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            SampleDataContext dataContext = new SampleDataContext();
+            GridView1.DataSource = from student in dataContext.Students
+                                   where student.Gender == "Male"
+                                   select student;
+            GridView1.DataBind();
+
+        }
+    }
+}
