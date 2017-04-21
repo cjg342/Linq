7b6d391309aeded2bda791f0564013bd5d53dcde
chris <chris@DEVBOX>
2017-04-20 23:32:48 -0700

    more

---
+++ Linq/Models/Student.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Linq.Models
+{
+    public class Students
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+
+        public static List<Students> GetAllStudents()
+        {
+            List<Students> listStudents = new List<Students>();
+
+            Students student1 = new Students
+            {
+                ID = 101,
+                Name = "Mark",
+                Gender = "Male"
+            };
+            listStudents.Add(student1);
+
+            Students student2 = new Students
+            {
+                ID = 102,
+                Name = "Mary",
+                Gender = "Female"
+            };
+            listStudents.Add(student2);
+
+            Students student3 = new Students
+            {
+                ID = 103,
+                Name = "John",
+                Gender = "Male"
+            };
+            listStudents.Add(student3);
+
+            Students student4 = new Students
+            {
+                ID = 104,
+                Name = "Steve",
+                Gender = "Male"
+            };
+            listStudents.Add(student4);
+
+            Students student5 = new Students
+            {
+                ID = 105,
+                Name = "Pam",
+                Gender = "Female"
+            };
+            listStudents.Add(student5);
+
+            return listStudents;
+        }
+    }
+}
---
+++ Linq/WebForm2.aspx.cs
@@ -0,0 +1,28 @@
+﻿using Linq.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Linq
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //LINQ query using using SQL like query expressions
+            IEnumerable<Students> students = from student in Students.GetAllStudents()
+                                             where student.Gender == "Male"
+                                             select student;
+            GridView1.DataSource = students;
+            GridView1.DataBind();
+
+            //LINQ query using Lambda Expressions.
+            IEnumerable<Students> students2 = Students.GetAllStudents()
+            .Where(student => student.Gender == "Male");
+
+            GridView2.DataSource = students2;
+            GridView2.DataBind();
+        }
+    }
+}
